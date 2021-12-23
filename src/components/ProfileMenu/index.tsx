274112dcb5922cc9a3742276e15bde282bbf3d74
Mike Santos <mike.santos@contraprova.com.br>
2021-12-23 15:25:21 -0300

    implementação e teste do component ProfileMenu

---
+++ src/components/ProfileMenu/index.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import {
+  AccountCircle,
+  CreditCard,
+  ExitToApp,
+  FormatListBulleted
+} from 'styled-icons/material-outlined'
+
+import * as S from './styles'
+
+export type ProfileMenuProps = {
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders'
+}
+
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
+  <S.Nav>
+    <Link href="/profile/me" passHref>
+      <S.Link title="My profile" isActive={activeLink === '/profile/me'}>
+        <AccountCircle size={24} />
+        <span>My Profile</span>
+      </S.Link>
+    </Link>
+    <Link href="/profile/cards" passHref>
+      <S.Link title="My Cards" isActive={activeLink === '/profile/cards'}>
+        <CreditCard size={24} />
+        <span>My Cards</span>
+      </S.Link>
+    </Link>
+    <Link href="/profile/orders" passHref>
+      <S.Link title="My orders" isActive={activeLink === '/profile/orders'}>
+        <FormatListBulleted size={24} />
+        <span>My Orders</span>
+      </S.Link>
+    </Link>
+    <Link href="/profile/logout" passHref>
+      <S.Link title="sign out">
+        <ExitToApp size={24} />
+        <span>Sign Out</span>
+      </S.Link>
+    </Link>
+  </S.Nav>
+)
+
+export default ProfileMenu
