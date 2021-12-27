8185e7ff627bc74aeb87724c4ee4ffbdec38a7c9
Mike Santos <mike.santos@contraprova.com.br>
2021-12-27 13:24:12 -0300

    implementação e teste do component CartDropdown

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,24 @@
+import { Story, Meta } from '@storybook/react'
+import CartDropdown, { CartDropdownProps } from '.'
+
+import cartListMock from 'components/CartList/mock'
+
+export default {
+  title: 'CartDropdown',
+  component: CartDropdown,
+  args: {
+    items: cartListMock,
+    total: 'R$ 440,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<CartDropdownProps> = (args) => (
+  <div style={{ float: 'right' }}>
+    <CartDropdown {...args} />
+  </div>
+)
