318b7aff8739ac7e42f1669deaeab507809e526f
Mike Santos <iddeiasimplesmike@gmail.com>
2022-02-10 11:42:45 -0300

    correção em testes de componentes e templates

--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -5,14 +5,18 @@ import ProfileMenu from '.'
 
 describe('<ProfileMenu />', () => {
   it('should render menu', () => {
-    render(<ProfileMenu />)
+    const { container } = render(<ProfileMenu />)
 
     expect(
       screen.getByRole('link', { name: /my profile/i })
     ).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
-    expect(screen.getByText(/sign out/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign out/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the menu with an active link defined', () => {
