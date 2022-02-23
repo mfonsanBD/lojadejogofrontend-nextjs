520e44a2dd7b2001d9e91562dcc63a50445c0a54
Mike Santos <iddeiasimplesmike@gmail.com>
2022-02-23 11:55:28 -0300

    incluindo a proteção de rotas na página de carrinho da loja

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -6,19 +6,23 @@ import itemsMock from 'components/CartList/mock'
 import cardsMock from 'components/PaymentOptions/mock'
 import { initializeApollo } from 'utils/apollo'
 import { GameMapper, HighlightMapper } from 'utils/mappers'
+import protectedRoutes from 'utils/protected-routes'
+import { GetServerSidePropsContext } from 'next'
 
 export default function Index(props: CartTemplateProps) {
   return <Cart {...props} />
 }
 
-export async function getStaticProps() {
-  const apolloClient = initializeApollo()
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
 
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
   })
   return {
     props: {
+      session,
       recommendedTitle: data.recommended?.section?.title,
       recommendedHighlight: HighlightMapper(
         data.recommended!.section!.highlight
